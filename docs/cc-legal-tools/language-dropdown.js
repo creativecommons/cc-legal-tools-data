3e2d46826088030b1f915c0dc2353c58e75c2ef9
chaneloco <ommarsolis1993@gmail.com>
2025-08-19 01:25:30 -0600

    Delete docs directory

--- docs/cc-legal-tools/language-dropdown.js
+++
@@ -1,7 +0,0 @@
-const select = document.getElementById("languages-dropdown")
-
-select.addEventListener("input", function () {
-    const language_code = select.value
-    const option = document.getElementById("option-" + language_code)
-    window.location.href = option.dataset.link
-})
